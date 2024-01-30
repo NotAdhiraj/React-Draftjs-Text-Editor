e4c4aad8204a26af51f5373ef85dfe923b71c1de
NotAdhiraj <adhiajsingh129@gmail.com>
2024-01-30 14:19:50 +0530

    before

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -43,10 +43,11 @@ function DemoEditor() {
 	}
 
 	useEffect(() => {
-	    let storedText = typeof window !== 'undefined' && window.localStorage ? localStorage.getItem("text") : null;
-	    let contentState = storedText ? stateFromMarkdown(storedText.toString()) : EditorState.createEmpty();
-	    let newEditorState = EditorState.push(editorState, contentState);
-	    setEditorState(newEditorState);
+		let contentState = stateFromMarkdown(
+			localStorage.getItem("text").toString()
+		);
+		let newEditorState = EditorState.push(editorState, contentState);
+		setEditorState(newEditorState);
 	}, []);
 
 	const onClick = () => {
